27398f41645266463c043a71b9120883c056b91c
BardieJoensen <bardie48@gmail.com>
2026-09-03 11:49:09 +0200

    complete python3 migration
    Makefile, requirements.txt and run_forever.sh still targeted python2

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -3,7 +3,7 @@ PYTHON_SCRIPT_PATH=$1
 
 while true
 do
-    python2 "$PYTHON_SCRIPT_PATH"
+    python3 "$PYTHON_SCRIPT_PATH"
     exit_code=$?
     if [ "$exit_code" -ne 0 ]; then
         echo "Script crashed with exit code $exit_code. Restarting..." >&2
